01459625ac12f7a94e007a15d461c04801ec4ee3
Birutes <birutepaplauskaite@yahoo.com>
2016-10-02 21:46:21 +0300

    Pakeitimai
    Issiaiskinau, kodel neveike jQuery, bet ne visi elementai keitesi, kai
    bandziau naudoti su css transition, pvz nesikeicia border-color

--- cinema2.js
+++ cinema2.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 	$('#first').on('click', function() {
 		$(this).toggleClass('animation1');
-	});
+			});
+
+	$('#second').on('click', function() {
+		$(this).toggleClass('animation2');
+			});
+
+	$('#third').on('click', function() {
+		$(this).toggleClass('animation3');
+			});
+
+	$('#fourth').on('click', function() {
+		$(this).toggleClass('animation4');
+			});
+
+	$('#fifth').on('click', function() {
+		$(this).toggleClass('animation5');
+			});
+
+	$('#sixth').on('click', function() {
+		$(this).toggleClass('animation6');
+			});
+
+	$('#seventh').on('click', function() {
+		$(this).toggleClass('animation7');
+			});
+
+	$('#eight').on('click', function() {
+		$(this).toggleClass('animation8');
+			});
+
+	$('#ninth').on('click', function() {
+		$(this).toggleClass('animation9');
+			});
+
+	$('#tenth').on('click', function() {
+		$(this).toggleClass('animation10');
+			});
 });
+	
