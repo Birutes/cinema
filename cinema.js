4c476f1690c948ae27c488635af2e5f4f2853145
Birutes <birutepaplauskaite@yahoo.com>
2016-10-14 12:10:30 +0300

    Pakeitimai

--- cinema.js
+++ cinema.js
@@ -1,41 +1,7 @@
 $(document).ready(function() {
-	$('#first').on('click', function() {
+	$('.square').on('click', function() {
 		var $this = $(this);
-		var animationType = $this.data("class");
+		var animationType = $this.data("animation");
 		$this.toggleClass(animationType);
-		$('#second').toggleClass('animation2');
 			});
-
-	$('#third').on('click', function() {
-		$(this).toggleClass('animation3');
-			});
-
-	$('#fourth').on('click', function() {
-		$(this).toggleClass('animation4');
-			});
-
-	$('#fifth').on('click', function() {
-		$(this).toggleClass('animation5');
-			});
-
-	$('#sixth').on('click', function() {
-		$(this).toggleClass('animation6');
-			});
-
-	$('#seventh').on('click', function() {
-		$(this).toggleClass('animation7');
-			});
-
-	$('#eight').on('click', function() {
-		$(this).toggleClass('animation8');
-			});
-
-	$('#ninth').on('click', function() {
-		$(this).toggleClass('animation9');
-			});
-
-	$('#tenth').on('click', function() {
-		$(this).toggleClass('animation10');
-			});
-});
 	
