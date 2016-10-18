28bc8a5d24053f60c0fc145364b5025ce6e44a69
Birutes <birutepaplauskaite@yahoo.com>
2016-10-18 15:37:57 +0300

    Ajax

--- cinema.js
+++ cinema.js
@@ -5,4 +5,27 @@ $(document).ready(function() {
 		$this.toggleClass(animationType);
 			});
 });
-	
+
+$(document).ready(function() {
+	$('.ajax').on('click', function() {
+		$.get( "data.txt", function( response ) {
+ console.log( response );
+ 		});
+	});
+
+
+
+$.getJSON("http://www.colourlovers.com/api/palettes/new?jsonCallback=?",
+ 	function(colorPalettes) {
+ 		printColors(colorPalettes);
+ 	}
+ );
+
+ function printColors(colorPalettes){
+ 		var color = "#" + colorPalettes[0].colors[0];
+
+        $(".ajax").on("click", function () {
+            $("p").css("color", color);
+        });
+    };
+});
